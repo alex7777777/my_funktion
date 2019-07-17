2ffea294cc30080cdecf125a03cd9290e199a521
alexgorbach <gorbach@cubematrix.com>
2019-07-17 13:58:58 +0200

    Add files via upload

---
+++ 39_function.R
@@ -0,0 +1,16 @@
+#######################################################
+# 2019-01-08 - by Alex Gorbach
+# DB Zugriff - BigQuery
+#######################################################
+
+# Function: SQL-Access to the BigQuery tables
+
+my_bigquery <- function(my_project, my_sql, my_sql_type = F) {
+  
+  library(bigrquery)
+  my_bigquery_df <- query_exec(my_sql, project = my_project, 
+                               use_legacy_sql = my_sql_type,
+                               max_pages = Inf) 
+  
+  return(my_bigquery_df)
+}
