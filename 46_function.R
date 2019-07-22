70e9d15f78b7ff67f7d3124dccdcf728b9d12996
alexgorbach <gorbach@cubematrix.com>
2019-07-22 11:24:08 +0200

    Update 46_function.R

--- 46_function.R
+++ 46_function.R
@@ -3,12 +3,13 @@
 # Generating of date strings                                               #
 # ######################################################################## #
 
-my_day_date <- function(date_from_char, number_days) {
-  
+my_day_date <- function(date_from_char, number_days, date_takt=1) {
   date_list <- list()
   date_list[1] <- c(date_from_char)
-  for (i in 2:(number_days)){
-    date_list[i] <- c(as.character(as.Date(date_list[[i-1]])+1))
+  if(floor(number_days/date_takt)>=2){
+    for (i in 2:floor(number_days/date_takt)){
+      date_list[i] <- c(as.character(as.Date(date_list[[i-1]])+date_takt))
+    }
   }
   return(date_list)
 }
