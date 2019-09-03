1a54463316e44511ca1790a94c2728cd5f453cb0
alexgorbach <gorbach@cubematrix.com>
2019-09-03 09:21:19 +0200

    Update 37_function2.R

--- 37_function2.R
+++ 37_function2.R
@@ -27,3 +27,65 @@ write_to_DWH <- function(object, table_name) {
                  append = TRUE)
   }
 }
+
+
+
+
+
+
+# #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# #          MAIN SCRIPT            ~
+# #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 
+# library(lubridate)
+# library(bigrquery)
+# library(jsonlite)
+# library(tidyr)
+# library(dplyr)
+# library(ggplot2)
+# library(httpuv)
+# library(RJDBC) # DB Connect
+# library(RODBC) # DB Connect
+# library(dbplyr)
+# library(stringr)
+# library('RCurl')
+# library(stringr)
+# library(readr)
+# 
+# options(digits = 2)
+# options("warning.length"=8000)
+# options(scipen = 999)
+# 
+# #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# #                DB Connection                   ~
+# #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# TD_USER <- Sys.getenv("TERA_USER")    # login    - vgl. function_38.R
+# TD_PW <- Sys.getenv("TERA_PW")        # password
+# 
+# HOMEDRIVE <- Sys.getenv("HOMEDRIVE")
+# HOMEPATH <- Sys.getenv("HOMEPATH")
+# HOME <- paste0(HOMEDRIVE, HOMEPATH)
+# 
+# # TODO: Adjust Path to Teradata Drivers
+# PATH_TERAJDBC <- paste0(HOME, "\\Software\\TeraJDBC__indep_indep.16.20.00.08\\terajdbc4.jar")
+# PATH_TERACONFIG <- paste0(HOME, "\\Software\\TeraJDBC__indep_indep.16.20.00.08\\tdgssconfig.jar")
+# PATH_TERA <- paste(PATH_TERAJDBC, PATH_TERACONFIG, sep=";")
+# 
+# # Setup JDBC Driver: WAY faster reading data than ODBC 
+# drv <- JDBC("com.teradata.jdbc.TeraDriver", PATH_TERA)
+# .jaddClassPath(PATH_TERACONFIG)
+# conn <- dbConnect(drv,
+#                   "jdbc:teradata://td2.eil.risnet.de/DATABASE=REWE_DIGITAL,DBS_PORT=1025,charSet=UTF8,LOB_SUPPORT=OFF",
+#                   TD_USER, TD_PW)
+# 
+# #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# #          Write BigQuery Data to DWH            ~
+# #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 
+# # the table to write: 'tbl_bigquery':
+# 
+# dbRemoveTable(conn, "OCMA_BIGQUERY")
+# dbExistsTable(conn, "OCMA_BIGQUERY")
+# 
+# write_to_DWH(object = tbl_bigquery,
+#              table_name = "OCMA_BIGQUERY")
