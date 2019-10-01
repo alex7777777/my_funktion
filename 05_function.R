9a0409bb7f4ee683ce1d1361b743a3eab09ea638
alexgorbach <gorbach@cubematrix.com>
2019-10-01 15:17:55 +0200

    Update 05_function.R

--- 05_function.R
+++ 05_function.R
@@ -1,6 +1,5 @@
 #######################################################
 # 2018-01-29 - by Alex Gorbach
-# 2019-09-16 Update for the time scaling
 # DB Datensatz Carsharing - Flinkster
 # Buchungen Carsharing 
 # http://data.deutschebahn.com/dataset/data-flinkster
@@ -26,21 +25,21 @@ my_time_diff <- function(df_ids, df_date, scaling_time="") {
   my_df$t2 <- as.POSIXlt(strptime(my_df$t2, "%Y-%m-%d %H:%M:%S"))
   my_df$t2[1:(nrow(my_df)-1)] <- my_df$Datum[2:nrow(my_df)] # displacement
   if(scaling_time == "S") {
-    my_df$delta_t <- as.numeric(round((my_df$t2 - my_df$Datum)*3600))                    # sec
+    my_df$delta_t <- as.numeric(round((my_df$t2 - my_df$Datum)))                               # sec
   } else if(scaling_time == "M") {
-    my_df$delta_t <- as.numeric(round((my_df$t2 - my_df$Datum)*60))                      # min
+    my_df$delta_t <- as.numeric(round((my_df$t2 - my_df$Datum)/60))                            # min
   } else if(scaling_time == "H") {
-    my_df$delta_t <- as.numeric(round(my_df$t2 - my_df$Datum))                           # hour
+    my_df$delta_t <- as.numeric(round((my_df$t2 - my_df$Datum)/(60*60), digits = 1))           # hour
   } else if(scaling_time == "d") {
-    my_df$delta_t <- as.numeric(round((my_df$t2 - my_df$Datum)/24))                      # days
+    my_df$delta_t <- as.numeric(round((my_df$t2 - my_df$Datum)/(24*60*60), digits = 1))        # days
   } else if(scaling_time == "w") {
-    my_df$delta_t <- as.numeric(round((my_df$t2 - my_df$Datum)/(24*7), digits = 1))      # week
+    my_df$delta_t <- as.numeric(round((my_df$t2 - my_df$Datum)/(24*60*60*7), digits = 1))      # week
   } else if(scaling_time == "m") {
-    my_df$delta_t <- as.numeric(round((my_df$t2 - my_df$Datum)/(24*30.5), digits = 1))   # month
+    my_df$delta_t <- as.numeric(round((my_df$t2 - my_df$Datum)/(24*60*60*30.5), digits = 1))   # month
   } else if(scaling_time == "q") {
-    my_df$delta_t <- as.numeric(round((my_df$t2 - my_df$Datum)/(24*3*30.5), digits = 1)) # quarter
+    my_df$delta_t <- as.numeric(round((my_df$t2 - my_df$Datum)/(24*60*60*3*30.5), digits = 1)) # quarter
   } else if(scaling_time == "y") {
-    my_df$delta_t <- as.numeric(round((my_df$t2 - my_df$Datum)/(24*365.25), digits = 1)) # year
+    my_df$delta_t <- as.numeric(round((my_df$t2 - my_df$Datum)/(24*60*60*365.25), digits = 1)) # year
   } else {
     my_df$delta_t <- 1
     print("The time difference for other formats is not defined here: dummy time")
