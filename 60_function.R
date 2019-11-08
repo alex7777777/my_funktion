737fe98d54a4b66f8dd46f6919cfe13113339b4e
alexgorbach <gorbach@cubematrix.com>
2019-11-08 16:44:15 +0100

    60_function.R
    Cutting sequences into parts ending with the selected event. Usually this is a dependent variable 'purchase'.

---
+++ 60_function.R
@@ -0,0 +1,102 @@
+#######################################################
+# 2019-11-08 - by Alex Gorbach
+#######################################################
+
+# Function: cutting sequences into parts ending with the selected event 
+#           Usually this is a dependent variable 'purchase'
+
+my_sequences_cutting <- function(DF, depend_var_name) {
+  
+  # Step 1: set int. parameter
+  ids_vector <- unique(DF$ids)
+  length_df <- nrow(DF)
+  DF$new_ids <- NA
+  
+  # Step 2: loop
+  for(i in 1:length(unique(DF$ids))) { 
+    
+    if(length(unique(DF$ids))>0) {
+      cat(paste0(round(100*length(unique(DF$ids))/(1+length(unique(DF$ids))-i)), "%"))
+    }
+    
+    DF2 <- DF[DF$ids %in% ids_vector[i], ]
+    
+    # Delete sale-duplicates 'depend_var_name' in one identifier
+    DF2$event2[2:nrow(DF2)] <- DF2$event[1:(nrow(DF2)-1)]
+    DF2 <- DF2[!((DF2$event==DF2$event2)&(DF2$event==depend_var_name)&(!is.na(DF2$event2))), ]
+    if(DF2[1, ]$event==depend_var_name) { DF2 <- DF2[2:nrow(DF2), ]}
+    
+    DF2$event2 <- NULL
+    
+    if(depend_var_name %in% DF2$event) {
+      DF2[DF2$event==depend_var_name,]$delta_t <- NA
+      DF2$new_ids <- rownames(DF2)
+      DF2$new_ids <- paste0(DF2$new_ids, "_", DF2$ids)
+      DF2[DF2$event!=depend_var_name,]$new_ids <- NA
+    } else {
+      DF2$new_ids[nrow(DF2)] <-  paste0(rownames(DF2)[nrow(DF2)], "_", DF2$ids[nrow(DF2)])
+    }
+    
+    # Fill gaps
+    DF2 <- DF2[order(-as.integer(rownames(DF2))), ]
+    DF2$new_ids <- fillNAgaps(DF2$new_ids)
+    DF2 <- DF2[order(as.integer(rownames(DF2))), ]
+    
+    DF <- rbind(DF, DF2)
+    
+  }
+  # End loop
+  
+  # Select new sequences
+  DF <- DF[!is.na(DF$new_ids), ]
+  rownames(DF) <- NULL
+  DF$new_ids <- as.integer(as.factor(DF$new_ids))
+  
+
+  # # Optional:
+  # save_ids_matching <- unique(DF[ , c("ids", "new_ids")])
+  # save_ids_matching <- save_ids_matching[order(save_ids_matching$ids, save_ids_matching$new_ids), ]
+  # save(save_ids_matching, file = "save_ids_matching.RObject")
+  
+  # Return DF
+  DF <- DF[ , c("ids", "new_ids", "Datum", "event", "delta_t")]
+  names(DF) <- c("old_ids", "ids", "Datum", "event", "delta_t")
+  # end
+  
+
+  return(DF)
+}
+
+fillNAgaps <- function(x, firstBack=FALSE) {
+  ## NA's in a vector or factor are replaced with last non-NA values
+  ## If firstBack is TRUE, it will fill in leading NA's with the first
+  ## non-NA value. If FALSE, it will not change leading NA's.
+  
+  # If it's a factor, store the level labels and convert to integer
+  lvls <- NULL
+  if (is.factor(x)) {
+    lvls <- levels(x)
+    x    <- as.integer(x)
+  }
+  
+  goodIdx <- !is.na(x)
+  
+  # These are the non-NA values from x only
+  # Add a leading NA or take the first good value, depending on firstBack   
+  if (firstBack)   goodVals <- c(x[goodIdx][1], x[goodIdx])
+  else             goodVals <- c(NA,            x[goodIdx])
+  
+  # Fill the indices of the output vector with the indices pulled from
+  # these offsets of goodVals. Add 1 to avoid indexing to zero.
+  fillIdx <- cumsum(goodIdx)+1
+  
+  x <- goodVals[fillIdx]
+  
+  # If it was originally a factor, convert it back
+  if (!is.null(lvls)) {
+    x <- factor(x, levels=seq_along(lvls), labels=lvls)
+  }
+  
+  x
+}
+
