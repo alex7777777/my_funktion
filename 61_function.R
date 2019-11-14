641bb0590ffe0fef3b8233408dfeacf3157ee8dc
alexgorbach <gorbach@cubematrix.com>
2019-11-14 14:56:06 +0100

    Update 61_function.R

--- 61_function.R
+++ 61_function.R
@@ -8,7 +8,9 @@
 
 # file structur: "ids", "Datum", "event"
 
-my_repeating_reduction <- function(my_sqa) {
+my_repeating_reduction <- function(my_sqa, event_reduct="") {
+  
+  if(class(my_sqa$event)!="character") { my_sqa$event <- as.character(my_sqa$event)}
   
   ncol_for_return <- ncol(my_sqa)
   lines_start <- nrow(my_sqa)
@@ -18,8 +20,16 @@ my_repeating_reduction <- function(my_sqa) {
   my_sqa$event3[2:nrow(my_sqa)] <- my_sqa$event[1:(nrow(my_sqa)-1)]
   my_sqa$event3[1] <- ""
   
-  my_sqa <- my_sqa[!((my_sqa$event==my_sqa$event2)
-                     &(my_sqa$event==my_sqa$event3)), ]
+  # default: reduction of all recurring events
+  if(event_retuct == "") {
+    my_sqa <- my_sqa[!((my_sqa$event==my_sqa$event2)
+                       &(my_sqa$event==my_sqa$event3)), ]
+  } else {
+  # reduction of only the selected event
+    my_sqa <- my_sqa[!((my_sqa$event==event_reduct)
+                       &(my_sqa$event==my_sqa$event2)
+                       &(my_sqa$event==my_sqa$event3)), ]
+  }
   
   cat(paste0(lines_start - nrow(my_sqa),
              " lines with recurring events have been deleted\n"))
