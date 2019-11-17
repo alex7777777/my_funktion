d6e693df03629e009f12bee163be69bb168b7b7d
alexgorbach <gorbach@cubematrix.com>
2019-11-17 23:43:50 +0100

    Update 58_function.R

--- 58_function.R
+++ 58_function.R
@@ -5,12 +5,16 @@
 
 my_dates_generator <- function(date_from, days_number)
 {
+  if(days_number < 1) { stop("The number of days must be at least one day") }
+  
   day_list <- c()
   day_list[1] <- date_from
   
-  for(i in 2:days_number) {
+  if(days_number >= 2) {
+    for(i in 2:days_number) {
     day_list[i] <- as.character(as.Date(day_list[i-1])+1)
-  }
+    }
+  } 
   
   return(day_list)
 }
