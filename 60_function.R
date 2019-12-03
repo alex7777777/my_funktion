a32c79e04f5d56f2f3d45a9906e7b9d2033002b2
alexgorbach <gorbach@cubematrix.com>
2019-12-03 13:51:48 +0100

    Update 60_function.R

--- 60_function.R
+++ 60_function.R
@@ -1,20 +1,29 @@
 #######################################################
-# 2019-11-08 - by Alex Gorbach
+# 2019-12-03 - by Alex Gorbach
 #######################################################
 
 # Function: cutting sequences into parts ending with the selected event 
 #           Usually this is a dependent variable 'purchase'
 
-my_sequences_cutting <- function(DF, cut_seq_event="Kauf") {
+# fancier text progress
+# install.packages("svMisc")
+require(svMisc)
+
+my_sequences_cutting <- function(DF, cut_seq_event="Kauf", save_Y_N="Y") {
   
   # Step 1: set int. parameter
   ids_vector <- unique(DF$ids)
   length_df <- nrow(DF)
+  length_uniq_id <- length(unique(DF$ids))
   DF$new_ids <- NA
+  t <- rep("=", length_uniq_id)
   
+  cut("\n")
   # Step 2: loop
-  for(i in 1:length(unique(DF$ids))) { 
-        
+  for(i in 1:length_uniq_id) { 
+    
+    progress(round(101*(i)/length_uniq_id))
+    
     DF2 <- DF[DF$ids %in% ids_vector[i], ]
     
     # Delete sale-duplicates 'cut_seq_event' in one identifier
@@ -41,21 +50,24 @@ my_sequences_cutting <- function(DF, cut_seq_event="Kauf") {
     DF <- rbind(DF, DF2)
     
   }
+  # End loop
   
   # Select new sequences
   DF <- DF[!is.na(DF$new_ids), ]
   rownames(DF) <- NULL
   DF$new_ids <- as.integer(as.factor(DF$new_ids))
   
-
-  # # Optional:
-  # save_ids_matching <- unique(DF[ , c("ids", "new_ids")])
-  # save_ids_matching <- save_ids_matching[order(save_ids_matching$ids, save_ids_matching$new_ids), ]
-  # save(save_ids_matching, file = "save_ids_matching.RObject")
+  # Optional:
+  if(save_Y_N=="Y") {
+    save_ids_matching <- unique(DF[ , c("ids", "new_ids")])
+    save_ids_matching <- save_ids_matching[order(save_ids_matching$ids, save_ids_matching$new_ids), ]
+    save(save_ids_matching, file = "save_ids_matching.RObject")
+  }
   
+  # Return DF
   DF <- DF[ , c("ids", "new_ids", "Datum", "event", "delta_t")]
   names(DF) <- c("old_ids", "ids", "Datum", "event", "delta_t")
-  
+
   return(DF)
 }
 
@@ -87,6 +99,6 @@ fillNAgaps <- function(x, firstBack=FALSE) {
   # If it was originally a factor, convert it back
   if (!is.null(lvls)) {
     x <- factor(x, levels=seq_along(lvls), labels=lvls)
-  }  
+  }
   x
 }
