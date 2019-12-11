a5ad03a287da90e2e47fa03af9b0ac8f5f6de403
alexgorbach <gorbach@cubematrix.com>
2019-12-11 16:45:42 +0100

    Clean strings for DF

--- 49_function.R
+++ 49_function.R
@@ -16,7 +16,7 @@ my_clean_strings <- function(df_before_clean, search_symb_char, insert_symb_char
   }
   
   df_after_clean <- data.frame(sapply(df_before_clean, function(x) as.character(x)), stringsAsFactors=F)
-  names(df_after_clean) <- names_before_clean
+  names(df_after_clean) <- names_before_clean 
   
   # df_after_clean <- data.frame(lapply(df_before_clean, function(x) as.character(x)))
   # df_before_clean <- data.frame(lapply(df_before_clean, function(x) ifelse(grepl(search_symb_char, x), insert_symb_char, gsub(search_symb_char, insert_symb_char, x))))
